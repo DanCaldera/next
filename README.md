960623b37d4681c77bd52175a198e5eddfed862c
Daniel <bdcaldera@gmail.com>
2021-06-12 13:40:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+## App Next/Node de ejemplo usando Docker
+
+Esta app muestra y permite editar un perfil de usuario.
+Incluye:
+
+- Template inicial de nextjs
+- Un archivo index.html con js y css inline
+- Un archivo server.js como backend nodejs usando express
+- Una conexión mongodb para guardar los datos
+
+## 4 Componentes
+
+El proyecto podría funcionar si instalamos Nextjs, Node, MongoDB y Mongo Express en nuestro servidor local.
+Pero la intención es no instalar ninguno de ellos y que funcione gracias a Docker.
+
+Para ello creamos 4 contenedores:
+
+- Un contenedor Nextjs que contiene el código base para crear el frontend
+- Un contenedor Node que contiene el código de nuestra app
+- Un contenedor de MongoDB asociado a un volumen
+- Y uno de Mongo Express para consultar nuestra BD a través de una UI
+
+## Cómo iniciar el proyecto
+
+### Usando comandos Docker
+
+Puedes revisar el archivo txt incluido en el repositorio y ejecutar cada uno de los comandos.
+
+Con estos comandos puedes:
+
+- Crear una imagen basada en Node con el código de la app.
+- Iniciar un contenedor de mongo y mongo-express
+- Definir las credenciales de mongo como variables de entorno
+- Configurar Mongo Express para que se conecte a Mongo DB
+- Asociar los contenedores a una Network
+- Exponer puertos para que estén disponibles en nuestro Host
+
+### Usando Docker Compose
+
+O bien puedes hacer todo lo anterior ejecutando un solo comando:
+
+    docker compose up
+
+## Una vez iniciado el proyecto
+
+- Puedes acceder a la app de next desde localhost:3000, esto aún está en modo de prueba para sustituir el mini front del pueto 8081
+
+- Puedes acceder a mongo-express desde localhost:8081 en tu navegador.
+  Desde allí puedes crear una base de datos "my-db" y una colección "users".
+
+- Puedes acceder a la aplicación desde localhost:8080 en tu navegador.
---
+++ node-app/server.js
@@ -0,0 +1,82 @@
+let express = require('express')
+let path = require('path')
+let fs = require('fs')
+let MongoClient = require('mongodb').MongoClient
+let bodyParser = require('body-parser')
+let app = express()
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, 'images/profile-1.jpg'))
+  res.writeHead(200, { 'Content-Type': 'image/jpg' })
+  res.end(img, 'binary')
+})
+
+// use when starting application locally
+// let mongoUrl = "mongodb://admin:password@localhost:27017";
+
+// use when starting application as docker container
+let mongoUrl = 'mongodb://admin:password@mongodb'
+
+const client = new MongoClient(mongoUrl, { useUnifiedTopology: true })
+const mongoConnection = client.connect()
+
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body
+
+  mongoConnection
+    .then(() => {
+      let db = client.db('my-db')
+      userObj['userid'] = 1
+
+      let myquery = { userid: 1 }
+      let newvalues = { $set: userObj }
+
+      db.collection('users').updateOne(myquery, newvalues, { upsert: true }, (err, res) => {
+        if (err) throw err
+      })
+    })
+    .catch(err => {
+      console.log('Error: ', err)
+    })
+
+  // Send response
+  res.send(userObj)
+})
+
+app.get('/get-profile', function (req, res) {
+  let response = {}
+
+  // Connect to the db
+  mongoConnection
+    .then(() => {
+      let db = client.db('my-db')
+
+      let myquery = { userid: 1 }
+
+      db.collection('users').findOne(myquery, (err, result) => {
+        if (err) throw err
+
+        // Send response
+        response = result
+        res.send(response ? response : {})
+      })
+    })
+    .catch(err => {
+      console.log('Error: ', err)
+    })
+})
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
